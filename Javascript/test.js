f355ddb2402adeac8f91357f5f6e3ff27311b50c
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-04-15 17:32:21 +0530

    Task 1 JS code

--- Javascript/test.js
+++
@@ -1,3 +0,0 @@
-let price = 1000, discount = 10, tax = 5;
-let discountedPrice = price - (price * discount / 100);
-let finalBill = discountedPrice + (discountedPrice * tax / 100); console.log("Final Bill Amount:", finalBill);
