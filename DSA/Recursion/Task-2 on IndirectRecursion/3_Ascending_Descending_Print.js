a9e33b5ed4b4143313b3daf0fbd40daefdb08af6
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-05-19 16:13:48 +0530

    Task2 on IndirectRecursion

--- DSA/Recursion/Task-2 on IndirectRecursion/3_Ascending_Descending_Print.js
+++ DSA/Recursion/Task-2 on IndirectRecursion/3_Ascending_Descending_Print.js
@@ -1,14 +1,14 @@
-function printAsc(n, i = 1) {
-  if (i > n) return;
+function printAsc(num, i = 1) {
+  if (i > num) return;
   console.log(i);
-  printDesc(n, i);
+  printDesc(num, i);
 }
 
-function printDesc(n, i) {
-  if (i >= n) return;
-  printAsc(n, i + 1);
+function printDesc(num, i) {
+  if (i >= num) return;
+  printAsc(num, i + 1);
   console.log(i);
 }
 
-let n = 3;
-printAsc(n);
+let num = 3;
+printAsc(num);
