2b8a37c99215947894594da48713367838dec0e9
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-06-02 16:58:55 +0530

    Binary search algorithm

---
+++ DSA/BinarySearchAlgo/test.js
@@ -0,0 +1,14 @@
+const binarySearch = (arr, target) => {
+  let start = 0;
+  let end = arr.length - 1;
+
+  console.log((end - start) / 2);
+
+  let mid = Math.floor((end + start) / 2);
+  console.log(mid);
+
+  //   while (start <= end) {
+  //   }
+};
+
+binarySearch([1, 2, 3, 4, 5, 7, 8, 9], 1);
